52bec8446a817b4b804eea7a3d1c64405d546334
levblanc <levblanc@gmail.com>
2024-02-18 18:21:03 +0800

    repo init

---
+++ README.md
@@ -0,0 +1,45 @@
+# py-langchain-integration
+
+## Python Version
+
+First, you must have the 3.11 version of Python installed:
+
+https://www.python.org/downloads/
+
+This is very important, as only a few versions of Python support LangChain and
+OpenAI.
+
+## Pipenv Installation and Configuration
+
+```zsh
+pip install pipenv
+
+# or
+
+pip3 install pipenv
+```
+
+## Install dependencies
+
+```zsh
+pipenv install
+```
+
+## Create and enter a new environment:
+
+```zsh
+pipenv shell
+```
+
+After doing this your terminal will now be running commands in this new
+environment managed by Pipenv.
+
+Once inside this shell, you can run Python commands. eg:
+
+```zsh
+python main.py
+```
+
+## env vars / keys update
+
+you need to exit the shell and re-enter using the `pipenv shell` command.
---
+++ main.py
@@ -0,0 +1,48 @@
+from langchain.llms import OpenAI
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain, SequentialChain
+from dotenv import load_dotenv
+import argparse
+
+load_dotenv()
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--task", default="return a list of numbers")
+parser.add_argument("--language", default="python")
+
+args = parser.parse_args()
+
+llm = OpenAI()
+
+code_prompt = PromptTemplate(
+    input_variables=["language", "task"],
+    template="Write a very short {language} function that will {task}",
+)
+
+test_prompt = PromptTemplate(
+    input_variables=["language", "code"],
+    template="Write a test for the following {language} in code: \n{code}",
+)
+
+code_chain = LLMChain(llm=llm, prompt=code_prompt, output_key="code")
+test_chain = LLMChain(llm=llm, prompt=test_prompt, output_key="test")
+
+# code_result = code_chain({"language": args.language, "task": args.task})
+# test_reslut = test_chain({"language": args.language, "code": code_result["code"]})
+
+chain = SequentialChain(
+    chains=[code_chain, test_chain],
+    input_variables=["task", "language"],
+    output_variables=["test", "code"],
+)
+
+result = chain({"language": args.language, "task": args.task})
+
+# print(code_result)
+# print(code_result["code"])
+# print(test_reslut["test"])
+print(">>>>>> GENERATED CODE:")
+print(result["code"])
+
+print(">>>>>> GENERATED TEST:")
+print(result["test"])
